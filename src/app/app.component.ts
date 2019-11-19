ace9f2e9c7cb9977896765fecc722d3daead6517
Romy Kusuma <romy.kusuma@gdplabs.id>
2019-11-19 14:27:15 +0700

    Add large class with static property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { LabelConstant } from './label.constant';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-cli839-sample';
+  title = LabelConstant.ABORT;
 }
